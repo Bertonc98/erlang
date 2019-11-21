eb80951a1667f9daab955ec10bf33c99c834c45c
Bertonc98 <bertonc98@gmail.com>
2019-11-21 18:49:45 +0100

    Two register on localhost

--- das.erl
+++ das.erl
@@ -1,13 +1,13 @@
 -module(das).
--export([start/0, store/3, stop/1, lookup/2, loop/2]).
+-export([start/0, store/3, stop/1, lookup/1, loop/2]).
 
 start()->register(node(), spawn(node(), ?MODULE, loop, [node(), []])).
 		 
-store(Tag, Value, Name) -> {Name, Name}!{store, {Tag, Value}}.
+store(Tag, Value, Name) -> {Name, Name}!{store, node(), {Tag, Value}}.
 
 stop(Server)->{Server, Server}!stop.
 
-lookup(Tag, Name)->{Name, Name}!{lookup, a@alberto, Tag}.
+lookup(Tag) ->{node(), node()}!{lookup, Tag}.
 
 is_member(Tag, Elm)->
 	case Elm of
@@ -17,10 +17,14 @@ is_member(Tag, Elm)->
 
 loop(Self, Stored)->
 	receive
-		{store, {Tag, Value}} -> io:format("Registered~n"), loop(Self, [{Tag,Value}|Stored]);
+		{store, a@alberto, {Tag, Value}} -> io:format("Registered~n"), 
+											{b@alberto, b@alberto}!{update, {Tag, Value}}, 
+											loop(Self, [{Tag,Value}|Stored]);
+		{store, b@alberto, {Tag, Value}} -> io:format("Registered~n"), 
+											{a@alberto, a@alberto}!{update, {Tag, Value}}, 
+											loop(Self, [{Tag,Value}|Stored]);
 		{update, {Tag, Value}} -> loop(Self, [{Tag,Value}|Stored]);	
-		{lookup, Client, Tag} -> io:format("~p---~p~n", [Client, [X||X<-Stored, is_member(Tag, X)]]), 
-								 {Client, Client}!{print, [X||X<-Stored, is_member(Tag, X)]}, 
+		{lookup, Tag} -> io:format("~p~n", [[X||X<-Stored, is_member(Tag, X)]]), 
 								 loop(Self, Stored);
 		{print, Message} -> io:format("~p~n", [Message]),
 							loop(Self, Stored);
